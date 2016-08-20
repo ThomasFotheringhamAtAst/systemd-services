7edd37436f6a27f1a08915e28ea6d19f476a6b72
Dario Caruso <darioyzf@gmail.com>
2016-08-20 19:11:50 +0200

    to be continued

--- index.ts
+++ index.ts
@@ -7,6 +7,13 @@ import * as child_process from 'child_process';
 const exec = child_process.exec;
 
 
+
+interface IStatus {
+    active: boolean;
+    active_status: string;
+}
+
+
 export default class SysService {
 
     constructor() {
@@ -14,9 +21,35 @@ export default class SysService {
     }
 
 
-    status(service: string) {
-        return new Promise<boolean>((resolve, reject) => {
+    status(service: string): Promise<IStatus> {
+        return new Promise<IStatus>((resolve, reject) => {
+            exec('systemctl status ' + service, (err, stdout, stderr) => {
+                if (stderr) {
+                    //   console.error(stderr)
+                }
+                if (err) {
+                    reject(err)
+                } else {
 
+                    let outperline = stdout.split('\n')
+
+                    const status = <IStatus>{}
+
+                    for (let i = 0; i < outperline.length; i++) {
+                        let line = outperline[i];
+
+                        if (line.split('Active:').length > 1) {
+                            status.active_status = line.split('Active: ')[1];
+                            if (status.active_status.split(' ')[0] === 'active') {
+                                status.active = true;
+                            }
+                        }
+
+                    }
+                    resolve(status)
+                }
+
+            })
         })
     }
 
---
+++ test/status.ts
@@ -0,0 +1,29 @@
+
+import SysService from '../index';
+
+import chai = require("chai");
+const expect = chai.expect;
+
+
+const ns = new SysService()
+
+
+
+describe("statuses", function () {
+    it("a working process return a status that is active", function (done) {
+        ns.status('networking').then((a: any) => {
+
+            expect(a).to.be.ok;
+            expect(a).to.be.an('object');
+            expect(a.active).to.be.a('boolean').that.is.eq(true);
+            expect(a.active_status).to.be.a('string');
+
+            done()
+
+        }).catch((err) => {
+            done(Error(err))
+        })
+    })
+})
+
+
